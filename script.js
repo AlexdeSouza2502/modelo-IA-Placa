9144882efe4797d4dcc9fc7c677a09681d6c2b4b
Alex_Souza2002 <62069087+AlexdeSouza2502@users.noreply.github.com>
2024-03-26 21:32:16 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,29 @@
-// Link para o seu modelo fornecido pelo painel de exportação do Teachable Machine
+  // Link para o seu modelo fornecido pelo painel de exportação do Teachable Machine
 const URL = "./placas_model/";
 
 let model, webcam, labelContainer, maxPredictions;
 
-// Função de conveniência para configurar uma webcam
+  // Variável para rastrear se a webcam já foi inicializada
+let webcamInitialized = false;
+
+  // Função de conveniência para configurar uma webcam
 async function init() {
-    // Verifica se a webcam já foi inicializada
-    if (!webcam) {
-        const modelURL = URL + "model.json";
+    if (!webcamInitialized) {
+        const modelURL    = URL + "model.json";
         const metadataURL = URL + "metadata.json";
 
-        model = await tmImage.load(modelURL, metadataURL);
+        model          = await tmImage.load(modelURL, metadataURL);
         maxPredictions = model.getTotalClasses();
 
-        const flip = true;
-        webcam = new tmImage.Webcam(200, 200, flip);
+        const flip   = true;
+              webcam = new tmImage.Webcam(200, 200, flip);
         await webcam.setup();
         await webcam.play();
         window.requestAnimationFrame(loop);
 
         document.getElementById("webcam-container").appendChild(webcam.canvas);
+
+        webcamInitialized = true;  // Marcar que a webcam foi inicializada
     }
 }
 
@@ -31,14 +35,14 @@ async function loop() {
 
 async function predict() {
     const prediction = await model.predict(webcam.canvas);
-    let maxProb = 0;
-    let maxName = "";
+    let   maxProb    = 0;
+    let   maxName    = "";
     for (let i = 0; i < maxPredictions; i++) {
         if (prediction[i].probability > maxProb) {
             maxProb = prediction[i].probability;
             maxName = prediction[i].className;
         }
     }
-    const classPrediction = maxName + ": " + (maxProb * 100).toFixed(2) + "%";
-    labelContainer.innerHTML = classPrediction;
+    const classPrediction          = maxName + ": " + (maxProb * 100).toFixed(2) + "%";
+          labelContainer.innerHTML = classPrediction;
 }
