50cf65bcba1f3e6ce0d3ddd12dbf261ad81ed8db
Alex_Souza2002 <62069087+AlexdeSouza2502@users.noreply.github.com>
2024-04-03 20:14:41 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,34 +1,29 @@
-// Link para o seu modelo fornecido pelo painel de exportação do Teachable Machine
+  // Link para o seu modelo fornecido pelo painel de exportação do Teachable Machine
 const URL = "./placas_model/";
 
 let model, webcam, labelContainer, maxPredictions;
 
-// Variável para rastrear se a webcam já foi inicializada
+  // Variável para rastrear se a webcam já foi inicializada
 let webcamInitialized = false;
 
-// Função de conveniência para configurar uma webcam
+  // Função de conveniência para configurar uma webcam
 async function init() {
     if (!webcamInitialized) {
-        const modelURL = URL + "model.json";
+        const modelURL    = URL + "model.json";
         const metadataURL = URL + "metadata.json";
 
-        model = await tmImage.load(modelURL, metadataURL);
+        model          = await tmImage.load(modelURL, metadataURL);
         maxPredictions = model.getTotalClasses();
 
-        const flip = true;
-        webcam = new tmImage.Webcam(200, 200, flip);
+        const flip   = true;
+              webcam = new tmImage.Webcam(200, 200, flip);
         await webcam.setup();
         await webcam.play();
         window.requestAnimationFrame(loop);
 
         document.getElementById("webcam-container").appendChild(webcam.canvas);
-        labelContainer = document.createElement("div");
-        document.getElementById("webcam-container").appendChild(labelContainer);
-        for (let i = 0; i < maxPredictions; i++) {
-            labelContainer.appendChild(document.createElement("div"));
-        }
 
-        webcamInitialized = true; // Marcar que a webcam foi inicializada
+        webcamInitialized = true;  // Marcar que a webcam foi inicializada
     }
 }
 
@@ -40,17 +35,14 @@ async function loop() {
 
 async function predict() {
     const prediction = await model.predict(webcam.canvas);
-    let maxProb = 0;
-    let maxName = "";
+    let   maxProb    = 0;
+    let   maxName    = "";
     for (let i = 0; i < maxPredictions; i++) {
         if (prediction[i].probability > maxProb) {
             maxProb = prediction[i].probability;
             maxName = prediction[i].className;
         }
     }
-    const classPrediction = maxName + ": " + (maxProb * 100).toFixed(2) + "%";
-    labelContainer.innerHTML = classPrediction;
+    const classPrediction          = maxName + ": " + (maxProb * 100).toFixed(2) + "%";
+          labelContainer.innerHTML = classPrediction;
 }
-
-// Chamar a função init uma vez que o documento foi completamente carregado
-document.addEventListener('DOMContentLoaded', init);
