c7f204590ead836170ea11389ee094f624184922
Alex_Souza2002 <62069087+AlexdeSouza2502@users.noreply.github.com>
2024-04-03 20:46:30 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,46 +3,46 @@ const URL = "./placas_model/";
 
 let model, webcam, labelContainer, maxPredictions;
 
-  // Variável para rastrear se a webcam já foi inicializada
-let webcamInitialized = false;
-
-  // Função de conveniência para configurar uma webcam
+// Carregar o modelo de imagem e configurar a webcam
 async function init() {
-    if (!webcamInitialized) {
-        const modelURL    = URL + "model.json";
-        const metadataURL = URL + "metadata.json";
+    const modelURL = URL + "model.json";
+    const metadataURL = URL + "metadata.json";
 
-        model          = await tmImage.load(modelURL, metadataURL);
-        maxPredictions = model.getTotalClasses();
+    // Carregar o modelo e metadados
+    model = await tmImage.load(modelURL, metadataURL);
+    maxPredictions = model.getTotalClasses();
 
-        const flip   = true;
-              webcam = new tmImage.Webcam(200, 200, flip);
-        await webcam.setup();
-        await webcam.play();
-        window.requestAnimationFrame(loop);
+    // Função de conveniência para configurar uma webcam
+    const flip = true; // se deve ou não virar a webcam
+    webcam = new tmImage.Webcam(200, 200, flip); // largura, altura, virar
+    await webcam.setup(); // solicitar acesso à webcam
+    await webcam.play();
+    window.requestAnimationFrame(loop);
 
-        document.getElementById("webcam-container").appendChild(webcam.canvas);
-
-        webcamInitialized = true;  // Marcar que a webcam foi inicializada
-    }
+    // anexar elementos ao DOM
+    document.getElementById("webcam-container").appendChild(webcam.canvas);
+    labelContainer = document.getElementById("label-container");
 }
 
 async function loop() {
-    webcam.update();
+    webcam.update(); // atualizar o quadro da webcam
     await predict();
     window.requestAnimationFrame(loop);
 }
 
+// Executar a imagem da webcam através do modelo de imagem
 async function predict() {
+    // prever pode receber uma imagem, vídeo ou elemento de canvas html
     const prediction = await model.predict(webcam.canvas);
-    let   maxProb    = 0;
-    let   maxName    = "";
+    console.log(prediction);
+    let maxProb = 0;
+    let maxName = "";
     for (let i = 0; i < maxPredictions; i++) {
-        if (prediction[i].probability > maxProb) {
+        if(prediction[i].probability > maxProb) {
             maxProb = prediction[i].probability;
             maxName = prediction[i].className;
         }
     }
-    const classPrediction          = maxName + ": " + (maxProb * 100).toFixed(2) + "%";
-          labelContainer.innerHTML = classPrediction;
+    const classPrediction = maxName + ": " + (maxProb * 100).toFixed(2) + "%"; // Exibindo a probabilidade em percentual
+    labelContainer.innerHTML = classPrediction;
 }
